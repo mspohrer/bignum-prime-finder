a572cc515bea741e56fee5f2e195afdc5f347775
Me <mspohrer@pdx.edu>
2019-07-19 18:25:21 -0700

    pipe tweaks and mem frees needed

---
+++ basic.c
@@ -0,0 +1,15 @@
+// I figure we'd want it to be pretty slow so it doesn't
+// process everything seemingly immediately.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int
+main(int argc, char *argv[])
+{
+  int start,stop;
+
+
+  
+}
